a9bfc06441d7f875dc17db993d69c351ad4c9024
Shakib448 <shakiba448@gmail.com>
2023-03-23 16:40:13 +0600

    WIP on native

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -34,6 +34,9 @@ const Popularjobs = () => {
           <FlatList
             data={[1, 2, 3, 4, 5, 6, 7, 8]}
             renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
           />
         )}
       </View>
