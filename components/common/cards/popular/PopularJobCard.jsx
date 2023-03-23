06fbe0fe2b67cc1232851090bbae7d62fa762425
Shakib448 <shakiba448@gmail.com>
2023-03-23 16:36:25 +0600

    WIP on native

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './popularjobcard.style'
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./popularjobcard.style";
 
 const PopularJobCard = () => {
   return (
     <View>
       <Text>PopularJobCard</Text>
     </View>
-  )
-}
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
