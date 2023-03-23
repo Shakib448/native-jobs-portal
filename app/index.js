2b54869e40abc8c7e0f6e47e2bbf904553623f69
Shakib448 <shakiba448@gmail.com>
2023-03-23 09:44:43 +0600

    WIP on native

--- app/index.js
+++ app/index.js
@@ -1,9 +1,40 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  ScreenHeaderBtn,
+  Welcome,
+  Nearbyjobs,
+  Popularjobs,
+} from "../components";
 
 const Home = () => {
-  <View>
-    <Text style={{ color: "#000" }}>Home</Text>
-  </View>;
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './screenheader.style'
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./screenheader.style";
 
 const ScreenHeaderBtn = () => {
   return (
     <View>
       <Text>ScreenHeaderBtn</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ScreenHeaderBtn
+export default ScreenHeaderBtn;
